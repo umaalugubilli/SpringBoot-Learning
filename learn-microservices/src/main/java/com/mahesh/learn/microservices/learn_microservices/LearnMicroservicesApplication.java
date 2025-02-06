4f7295b0c47aaef846409f5e1b9789a28bfb3deb
umaalugubillli <maheshalugubilli@gmail.com>
2025-02-07 00:00:49 +0530

    Intial Commit

---
+++ learn-microservices/src/main/java/com/mahesh/learn/microservices/learn_microservices/LearnMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.mahesh.learn.microservices.learn_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnMicroservicesApplication.class, args);
+	}
+
+}
